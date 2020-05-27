2d02c82eb8a3d282dccb45b3409946e770175e49
tracht <marija.trachtenberg@gmail.com>
2020-05-27 14:34:28 +0100

    create Note class and passes test

---
+++ spec/noteSpec.js
@@ -0,0 +1,15 @@
+
+describe("new note", function() {
+
+  var note
+  var body1 = "Make sure to bring a tent this weekend"
+
+  beforeEach(function(){
+    note = new Note(body1)
+  })
+
+  it("returns 'Make sure to bring a tent this weekend'", function() {
+    expect(note.body).toEqual(body1)
+  })
+
+})
