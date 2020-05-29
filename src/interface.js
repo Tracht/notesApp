89fe0d0145fb359d9a98dafebe4e90c9b6fdcbbe
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-29 15:25:21 +0100

    added to readme

--- src/interface.js
+++ src/interface.js
@@ -34,9 +34,8 @@ function createNewNote(event) {
 function addShowBodyEvent(noteElement, indexVar) {
   noteElement.addEventListener('click', function(event) {
     event.preventDefault();
-     let noteBody = document.getElementById('note-body')
-     noteBody.innerHTML = noteManager.getNote(indexVar).body
-
+      let noteBody = document.getElementById('note-body')
+      noteBody.innerHTML = noteManager.getNote(indexVar).body
     pageOneView.style.display = 'none'
     pageTwoView.style.display = 'block'
   })
